ef7bb023207123e84298fede44e10785d8387dfa
yomisana <sammywhy3236@gmail.com>
2022-06-05 20:45:42 +0800

    1.0.0-5 - 修正 客戶端關閉時 未把先前遊玩的召喚師資料清除 - 在檢查的部分添加防錯誤出現影響使用者 - 當異常檢查錯誤時將會啟動 重新檢查客戶端是否鍵在

--- src/core/index.js
+++ src/core/index.js
@@ -74,6 +74,15 @@ const $ = {
                     game_path = "";
                     game_dir = "";
                     ml_main.webContents.send("game_is_found", "尚未找到 LOL 客戶端");
+                    // summoner
+                    ml_main.webContents.send("summoner_name", "Waiting...");
+                    ml_main.webContents.send("summoner_level", "Waiting...");
+                    ml_main.webContents.send("summoner_status", "Waiting...");
+                    ml_main.webContents.send("summoner_status_message", "Waiting...");
+                    ml_main.webContents.send("summoner_masteryScore", "Waiting...");
+                    ml_main.webContents.send("summoner_region", "Waiting...");
+                    // game status
+                    ml_main.webContents.send("game_status", "Waiting...");
                     game_is_found = false;game_is_notfound = true;
                     first.reschedule(5 * 1000);
                 }
@@ -181,108 +190,113 @@ const $ = {
             }
         },
             function(err, httpResponse, body){
-                gameflow = body.replace(/[^A-Z0-9]/ig,"");
-                if(gameflow == "None"){
-                    ml_main.webContents.send("game_status", gameflow + " | 你目前可能在首頁或是選擇模式大廳");
-                }else if(gameflow == "Lobby"){
-                    if(me.lol.gameQueueType == "NORMAL"){
-                        ml_main.webContents.send("game_status", gameflow + " | 一般盲選對戰房間");
-                    }else if(me.lol.gameQueueType == "RANKED_SOLO_5x5"){
-                        ml_main.webContents.send("game_status", gameflow + " | 單雙積分對戰房間");
-                    }else if(me.lol.gameQueueType == "RANKED_FLEX_SR"){
-                        ml_main.webContents.send("game_status", gameflow + " | 彈性積分對戰房間");
-                    }else if(me.lol.gameQueueType == "NONE"){
-                        ml_main.webContents.send("game_status", gameflow + " | 自訂對戰對戰房間");
-                    }else if(me.lol.gameQueueType == "ARAM_UNRANKED_5x5"){
-                        ml_main.webContents.send("game_status", gameflow + " | 一般隨機單中對戰房間");
-                    }else if(me.lol.gameQueueType == "NORMAL_TFT"){
-                        ml_main.webContents.send("game_status", gameflow + " | 一般戰旗對戰房間");
-                    }else if(me.lol.gameQueueType == "RANKED_TFT"){
-                        ml_main.webContents.send("game_status", gameflow + " | 戰旗積分對戰房間");
-                    }else if(me.lol.gameQueueType == "RANKED_TFT_TURBO"){
-                        ml_main.webContents.send("game_status", gameflow + " | 超級抽抽樂戰旗對戰房間");
-                    }else if(me.lol.gameQueueType == "RANKED_TFT_PAIRS"){
-                        ml_main.webContents.send("game_status", gameflow + " | 雙人搭檔工作房對戰房間");
-                    }else if(me.lol.gameQueueType == "URF"){
-                        ml_main.webContents.send("game_status", gameflow + " | 阿福快打對戰房間");
-                    }else if(me.lol.gameQueueType == "BOT"){
-                        ml_main.webContents.send("game_status", gameflow + " | 玩家打電腦對戰房間");
-                    }else if(me.lol.gameQueueType == "PRACTICETOOL"){
-                        ml_main.webContents.send("game_status", gameflow + " | 練習工具對戰房間");
-                    }else{
-                        ml_main.webContents.send("game_status", gameflow + " | Waiting Play...");
+                try{
+                    gameflow = body.replace(/[^A-Z0-9]/ig,"");
+                    if(gameflow == "None"){
+                        ml_main.webContents.send("game_status", gameflow + " | 你目前可能在首頁或是選擇模式大廳");
+                    }else if(gameflow == "Lobby"){
+                        if(me.lol.gameQueueType == "NORMAL"){
+                            ml_main.webContents.send("game_status", gameflow + " | 一般盲選對戰房間");
+                        }else if(me.lol.gameQueueType == "RANKED_SOLO_5x5"){
+                            ml_main.webContents.send("game_status", gameflow + " | 單雙積分對戰房間");
+                        }else if(me.lol.gameQueueType == "RANKED_FLEX_SR"){
+                            ml_main.webContents.send("game_status", gameflow + " | 彈性積分對戰房間");
+                        }else if(me.lol.gameQueueType == "NONE"){
+                            ml_main.webContents.send("game_status", gameflow + " | 自訂對戰對戰房間");
+                        }else if(me.lol.gameQueueType == "ARAM_UNRANKED_5x5"){
+                            ml_main.webContents.send("game_status", gameflow + " | 一般隨機單中對戰房間");
+                        }else if(me.lol.gameQueueType == "NORMAL_TFT"){
+                            ml_main.webContents.send("game_status", gameflow + " | 一般戰旗對戰房間");
+                        }else if(me.lol.gameQueueType == "RANKED_TFT"){
+                            ml_main.webContents.send("game_status", gameflow + " | 戰旗積分對戰房間");
+                        }else if(me.lol.gameQueueType == "RANKED_TFT_TURBO"){
+                            ml_main.webContents.send("game_status", gameflow + " | 超級抽抽樂戰旗對戰房間");
+                        }else if(me.lol.gameQueueType == "RANKED_TFT_PAIRS"){
+                            ml_main.webContents.send("game_status", gameflow + " | 雙人搭檔工作房對戰房間");
+                        }else if(me.lol.gameQueueType == "URF"){
+                            ml_main.webContents.send("game_status", gameflow + " | 阿福快打對戰房間");
+                        }else if(me.lol.gameQueueType == "BOT"){
+                            ml_main.webContents.send("game_status", gameflow + " | 玩家打電腦對戰房間");
+                        }else if(me.lol.gameQueueType == "PRACTICETOOL"){
+                            ml_main.webContents.send("game_status", gameflow + " | 練習工具對戰房間");
+                        }else{
+                            ml_main.webContents.send("game_status", gameflow + " | Waiting Play...");
+                        }
+                    }else if(gameflow == "Matchmaking"){
+                        if(me.lol.gameQueueType == "NORMAL"){
+                            ml_main.webContents.send("game_status", gameflow + " | 匹配一般盲選");
+                        }else if(me.lol.gameQueueType == "RANKED_SOLO_5x5"){
+                            ml_main.webContents.send("game_status", gameflow + " | 匹配單雙積分");
+                        }else if(me.lol.gameQueueType == "RANKED_FLEX_SR"){
+                            ml_main.webContents.send("game_status", gameflow + " | 匹配彈性積分");
+                        }else if(me.lol.gameQueueType == "NONE"){
+                            ml_main.webContents.send("game_status", gameflow + " | 匹配自訂對戰");
+                        }else if(me.lol.gameQueueType == "ARAM_UNRANKED_5x5"){
+                            ml_main.webContents.send("game_status", gameflow + " | 匹配一般隨機單中");
+                        }else if(me.lol.gameQueueType == "NORMAL_TFT"){
+                            ml_main.webContents.send("game_status", gameflow + " | 匹配一般戰旗");
+                        }else if(me.lol.gameQueueType == "RANKED_TFT"){
+                            ml_main.webContents.send("game_status", gameflow + " | 匹配戰旗積分");
+                        }else if(me.lol.gameQueueType == "RANKED_TFT_TURBO"){
+                            ml_main.webContents.send("game_status", gameflow + " | 匹配超級抽抽樂戰旗");
+                        }else if(me.lol.gameQueueType == "RANKED_TFT_PAIRS"){
+                            ml_main.webContents.send("game_status", gameflow + " | 匹配雙人搭檔工作房");
+                        }else if(me.lol.gameQueueType == "URF"){
+                            ml_main.webContents.send("game_status", gameflow + " | 匹配阿福快打");
+                        }else if(me.lol.gameQueueType == "BOT"){
+                            ml_main.webContents.send("game_status", gameflow + " | 匹配玩家打電腦");
+                        }else if(me.lol.gameQueueType == "PRACTICETOOL"){
+                            ml_main.webContents.send("game_status", gameflow + " | 匹配練習工具");
+                        }else{
+                            ml_main.webContents.send("game_status", gameflow + " | 你目前在匹配列隊中?? 這...你辦不到的!!如果可以教我拜託了!!不可能是BUG真的!!");
+                        }
+                    }else if(gameflow == "ReadyCheck"){ // 自動接受
+                        ml_main.webContents.send("game_status", gameflow + " | 對戰正在自動接受中...");
+                        console.log("[INFO] 傳送自動接受資訊中...");
+                        $.post_accept();
+                    }else if(gameflow == "ChampSelect"){
+                        if(me.lol.gameQueueType == "NORMAL"){
+                            ml_main.webContents.send("game_status", gameflow + " | 一般盲選 選擇英雄腳色中");
+                        }else if(me.lol.gameQueueType == "RANKED_SOLO_5x5"){
+                            ml_main.webContents.send("game_status", gameflow + " | 單雙積分 選擇英雄腳色中");
+                        }else if(me.lol.gameQueueType == "RANKED_FLEX_SR"){
+                            ml_main.webContents.send("game_status", gameflow + " | 彈性積分 選擇英雄腳色中");
+                        }else if(me.lol.gameQueueType == "NONE"){
+                            ml_main.webContents.send("game_status", gameflow + " | 自訂對戰 選擇英雄腳色中");
+                        }else if(me.lol.gameQueueType == "ARAM_UNRANKED_5x5"){
+                            ml_main.webContents.send("game_status", gameflow + " | 一般隨機單中 選擇英雄腳色中");
+                        }else if(me.lol.gameQueueType == "NORMAL_TFT"){
+                            ml_main.webContents.send("game_status", gameflow + " | 一般戰旗 選擇英雄腳色中(??)");
+                        }else if(me.lol.gameQueueType == "RANKED_TFT"){
+                            ml_main.webContents.send("game_status", gameflow + " | 戰旗積分 選擇英雄腳色中(??)");
+                        }else if(me.lol.gameQueueType == "RANKED_TFT_TURBO"){
+                            ml_main.webContents.send("game_status", gameflow + " | 超級抽抽樂戰旗 選擇英雄腳色中(??)");
+                        }else if(me.lol.gameQueueType == "RANKED_TFT_PAIRS"){
+                            ml_main.webContents.send("game_status", gameflow + " | 雙人搭檔工作房 選擇英雄腳色中(??)");
+                        }else if(me.lol.gameQueueType == "URF"){
+                            ml_main.webContents.send("game_status", gameflow + " | 阿福快打 選擇英雄腳色中");
+                        }else if(me.lol.gameQueueType == "BOT"){
+                            ml_main.webContents.send("game_status", gameflow + " | 玩家打電腦 選擇英雄腳色中");
+                        }else if(me.lol.gameQueueType == "PRACTICETOOL"){
+                            ml_main.webContents.send("game_status", gameflow + " | 練習工具 選擇英雄腳色中");
+                        }else{
+                            ml_main.webContents.send("game_status", gameflow + " | 你目前正在選擇英雄腳色中?? 這...你辦不到的!!如果可以教我拜託了!!不可能是BUG真的!!");
+                        }
+                    }else if(gameflow == "InProgress"){
+                        ml_main.webContents.send("game_status", gameflow + " | 遊玩中...");
+                    }else if(gameflow == "Reconnect"){
+                        ml_main.webContents.send("game_status", gameflow + " | 遊戲還在進行中...請盡快連接回去!");
+                        $.post_reconnect(); // 立即重連
+                    }else if(gameflow == "PreEndOfGame"){
+                        ml_main.webContents.send("game_status", gameflow + " | 誰 Carry 這場 或是 態度佳的隊友給他一個選項吧!");
+                        $.kill_game(); // 預防再次重連
+                    }else if(gameflow == "EndOfGame"){
+                        $.kill_game(); // 預防再次重連
+                        ml_main.webContents.send("game_status", gameflow + " | 遊戲結束...下一場吧");
                     }
-                }else if(gameflow == "Matchmaking"){
-                    if(me.lol.gameQueueType == "NORMAL"){
-                        ml_main.webContents.send("game_status", gameflow + " | 匹配一般盲選");
-                    }else if(me.lol.gameQueueType == "RANKED_SOLO_5x5"){
-                        ml_main.webContents.send("game_status", gameflow + " | 匹配單雙積分");
-                    }else if(me.lol.gameQueueType == "RANKED_FLEX_SR"){
-                        ml_main.webContents.send("game_status", gameflow + " | 匹配彈性積分");
-                    }else if(me.lol.gameQueueType == "NONE"){
-                        ml_main.webContents.send("game_status", gameflow + " | 匹配自訂對戰");
-                    }else if(me.lol.gameQueueType == "ARAM_UNRANKED_5x5"){
-                        ml_main.webContents.send("game_status", gameflow + " | 匹配一般隨機單中");
-                    }else if(me.lol.gameQueueType == "NORMAL_TFT"){
-                        ml_main.webContents.send("game_status", gameflow + " | 匹配一般戰旗");
-                    }else if(me.lol.gameQueueType == "RANKED_TFT"){
-                        ml_main.webContents.send("game_status", gameflow + " | 匹配戰旗積分");
-                    }else if(me.lol.gameQueueType == "RANKED_TFT_TURBO"){
-                        ml_main.webContents.send("game_status", gameflow + " | 匹配超級抽抽樂戰旗");
-                    }else if(me.lol.gameQueueType == "RANKED_TFT_PAIRS"){
-                        ml_main.webContents.send("game_status", gameflow + " | 匹配雙人搭檔工作房");
-                    }else if(me.lol.gameQueueType == "URF"){
-                        ml_main.webContents.send("game_status", gameflow + " | 匹配阿福快打");
-                    }else if(me.lol.gameQueueType == "BOT"){
-                        ml_main.webContents.send("game_status", gameflow + " | 匹配玩家打電腦");
-                    }else if(me.lol.gameQueueType == "PRACTICETOOL"){
-                        ml_main.webContents.send("game_status", gameflow + " | 匹配練習工具");
-                    }else{
-                        ml_main.webContents.send("game_status", gameflow + " | 你目前在匹配列隊中?? 這...你辦不到的!!如果可以教我拜託了!!不可能是BUG真的!!");
-                    }
-                }else if(gameflow == "ReadyCheck"){ // 自動接受
-                    ml_main.webContents.send("game_status", gameflow + " | 對戰正在自動接受中...");
-                    console.log("[INFO] 傳送自動接受資訊中...");
-                    $.post_accept();
-                }else if(gameflow == "ChampSelect"){
-                    if(me.lol.gameQueueType == "NORMAL"){
-                        ml_main.webContents.send("game_status", gameflow + " | 一般盲選 選擇英雄腳色中");
-                    }else if(me.lol.gameQueueType == "RANKED_SOLO_5x5"){
-                        ml_main.webContents.send("game_status", gameflow + " | 單雙積分 選擇英雄腳色中");
-                    }else if(me.lol.gameQueueType == "RANKED_FLEX_SR"){
-                        ml_main.webContents.send("game_status", gameflow + " | 彈性積分 選擇英雄腳色中");
-                    }else if(me.lol.gameQueueType == "NONE"){
-                        ml_main.webContents.send("game_status", gameflow + " | 自訂對戰 選擇英雄腳色中");
-                    }else if(me.lol.gameQueueType == "ARAM_UNRANKED_5x5"){
-                        ml_main.webContents.send("game_status", gameflow + " | 一般隨機單中 選擇英雄腳色中");
-                    }else if(me.lol.gameQueueType == "NORMAL_TFT"){
-                        ml_main.webContents.send("game_status", gameflow + " | 一般戰旗 選擇英雄腳色中(??)");
-                    }else if(me.lol.gameQueueType == "RANKED_TFT"){
-                        ml_main.webContents.send("game_status", gameflow + " | 戰旗積分 選擇英雄腳色中(??)");
-                    }else if(me.lol.gameQueueType == "RANKED_TFT_TURBO"){
-                        ml_main.webContents.send("game_status", gameflow + " | 超級抽抽樂戰旗 選擇英雄腳色中(??)");
-                    }else if(me.lol.gameQueueType == "RANKED_TFT_PAIRS"){
-                        ml_main.webContents.send("game_status", gameflow + " | 雙人搭檔工作房 選擇英雄腳色中(??)");
-                    }else if(me.lol.gameQueueType == "URF"){
-                        ml_main.webContents.send("game_status", gameflow + " | 阿福快打 選擇英雄腳色中");
-                    }else if(me.lol.gameQueueType == "BOT"){
-                        ml_main.webContents.send("game_status", gameflow + " | 玩家打電腦 選擇英雄腳色中");
-                    }else if(me.lol.gameQueueType == "PRACTICETOOL"){
-                        ml_main.webContents.send("game_status", gameflow + " | 練習工具 選擇英雄腳色中");
-                    }else{
-                        ml_main.webContents.send("game_status", gameflow + " | 你目前正在選擇英雄腳色中?? 這...你辦不到的!!如果可以教我拜託了!!不可能是BUG真的!!");
-                    }
-                }else if(gameflow == "InProgress"){
-                    ml_main.webContents.send("game_status", gameflow + " | 遊玩中...");
-                }else if(gameflow == "Reconnect"){
-                    ml_main.webContents.send("game_status", gameflow + " | 遊戲還在進行中...請盡快連接回去!");
-                    $.post_reconnect(); // 立即重連
-                }else if(gameflow == "PreEndOfGame"){
-                    ml_main.webContents.send("game_status", gameflow + " | 誰 Carry 這場 或是 態度佳的隊友給他一個選項吧!");
-                    $.kill_game(); // 預防再次重連
-                }else if(gameflow == "EndOfGame"){
-                    $.kill_game(); // 預防再次重連
-                    ml_main.webContents.send("game_status", gameflow + " | 遊戲結束...下一場吧");
+                }catch(error){
+                    console.error("[ERROR - get_status] "+err);
+                    game_is_found = false;game_is_notfound = true;
                 }
             }
         );
